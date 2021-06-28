22086ad18d7deaba2bc2b801ccad848772109e74
Nikk Dusky <53310585+NikkDusky@users.noreply.github.com>
2021-06-28 09:40:59 +0500

    v 1.0.1
    UPD. Program can check status of the process. Running Ableton or not.

--- ableton_presence.py
+++ ableton_presence.py
@@ -4,6 +4,8 @@ import ctypes
 import time
 import os
 
+os.system("title Ableton Live 11 Sutie - Discord Presence")
+
 EnumWindows = ctypes.windll.user32.EnumWindows
 EnumWindowsProc = ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int))
 GetWindowText = ctypes.windll.user32.GetWindowTextW
@@ -13,6 +15,8 @@ IsWindowVisible = ctypes.windll.user32.IsWindowVisible
 application_id = 858730122290003988
 
 title = ""
+program = None
+project = None
 
 btns = [
     {
@@ -23,38 +27,45 @@ btns = [
 
 RPC = Presence(application_id)
 RPC.connect()
-os.system("Ableton Live 11 Suite Presence")
 start_time = time.time()
 
 while True:
-    def foreach_window(hwnd, lParam):
-        global title
-        if IsWindowVisible(hwnd):
-            length = GetWindowTextLength(hwnd)
-            buff = ctypes.create_unicode_buffer(length + 1)
-            GetWindowText(hwnd, buff, length + 1)
-            
-            if buff.value[-21:] == "Ableton Live 11 Suite":
-                title = buff.value
+    tmp = os.popen("tasklist").read()
+    if "Ableton Live 11 Suite" in tmp:
+        os.system("cls")
+        print("Ableton Live 11 Suite is running...")
+        def foreach_window(hwnd, lParam):
+            global title
+            if IsWindowVisible(hwnd):
+                length = GetWindowTextLength(hwnd)
+                buff = ctypes.create_unicode_buffer(length + 1)
+                GetWindowText(hwnd, buff, length + 1)
                 
-        return True
-        
-    EnumWindows(EnumWindowsProc(foreach_window), 0)
+                if buff.value[-21:] == "Ableton Live 11 Suite":
+                    title = buff.value
+                    
+            return True
+            
+        EnumWindows(EnumWindowsProc(foreach_window), 0)
 
-    title = title.split("-")
-    program = (title[1])[1:]
-    project = (title[0])[:-1]
-    
-    os.system("cls")
-  
-    print(f"Ableton: {program}")
-    print(f"Project: {project}")
+        title = title.split("-")
+        try:
+            program = (title[1])[1:]
+            project = (title[0])[:-1]
+        except IndexError:
+            print("Index Error Catched...")
+      
+        print(f"Ableton: {program}")
+        print(f"Project: {project}")
 
-    RPC.update(
-        state=f"{project}",
-        details="Project",
-        buttons=btns,
-        large_image="icon",
-        start=start_time
-        )
+        RPC.update(
+            state=f"{project}",
+            details="Project",
+            buttons=btns,
+            large_image="icon",
+            start=start_time
+            )
+    else:
+        os.system("cls")
+        print("Ableton Live 11 Suite not found...\nPlease, launch Ableton Live 11 Suite...")
     time.sleep(10)
